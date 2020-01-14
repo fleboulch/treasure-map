af8fe44fb29a87a659078e10ac5b1d79a7293735
fleboulch <florent.le.boulch@gmail.com>
2020-01-14 20:21:15 +0100

    fixup! Map domain

--- src/test/java/com/fleboulch/treasuremap/map/domain/TreasureBoxTest.java
+++ src/test/java/com/fleboulch/treasuremap/map/domain/TreasureBoxTest.java
@@ -1,8 +1,6 @@
 package com.fleboulch.treasuremap.map.domain;
 
-import com.fleboulch.treasuremap.kernel.exceptions.NegativeAttributeException;
 import com.fleboulch.treasuremap.kernel.exceptions.NegativeOrZeroAttributeException;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
