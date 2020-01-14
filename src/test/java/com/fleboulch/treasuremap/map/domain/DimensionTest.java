b3db52bc66314ee9ad6d1fec2c7d8bca077f58c6
fleboulch <florent.le.boulch@gmail.com>
2020-01-14 20:09:04 +0100

    fixup! Map domain

--- src/test/java/com/fleboulch/treasuremap/map/domain/DimensionTest.java
+++ src/test/java/com/fleboulch/treasuremap/map/domain/DimensionTest.java
@@ -1,8 +1,13 @@
 package com.fleboulch.treasuremap.map.domain;
 
-import org.assertj.core.api.Assertions;
+import com.fleboulch.treasuremap.kernel.exceptions.DomainException;
+import com.fleboulch.treasuremap.kernel.exceptions.NegativeIndexException;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DimensionTest {
 
@@ -14,9 +19,29 @@ class DimensionTest {
     })
     void a_dimension_should_have_positive_width_and_height(int width, int height) {
         Dimension dimension = new Dimension(new Width(width), new Height(height));
-        Assertions.assertThat(dimension.width().value()).isEqualTo(width);
-        Assertions.assertThat(dimension.height().value()).isEqualTo(height);
+        assertThat(dimension.width().value()).isEqualTo(width);
+        assertThat(dimension.height().value()).isEqualTo(height);
+    }
 
+    @ParameterizedTest
+    @CsvSource(value = {
+            "-2,-2",
+            "-4,3",
+            "3,-4"
+    })
+    void a_dimension_with_negative_width_or_height_cannot_be_created(int width, int height) {
+
+        assertThatThrownBy(
+                () -> new Dimension(new Width(width), new Height(height))
+        ).isInstanceOf(NegativeIndexException.class);
+    }
+
+    @Test
+    void a_dimension_with_null_width_and_height_cannot_be_created() {
+
+        assertThatThrownBy(
+                () -> new Dimension(null, null)
+        ).isInstanceOf(DomainException.class);
     }
 
 }
