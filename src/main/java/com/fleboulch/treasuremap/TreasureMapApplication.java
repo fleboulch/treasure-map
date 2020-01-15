118126c41096c2074e263781706e370cb4de8fea
fleboulch <florent.le.boulch@gmail.com>
2020-01-15 22:17:04 +0100

    Make app build

---
+++ src/main/java/com/fleboulch/treasuremap/TreasureMapApplication.java
@@ -0,0 +1,13 @@
+package com.fleboulch.treasuremap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasureMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreasureMapApplication.class, args);
+    }
+
+}
