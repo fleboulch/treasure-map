02052211f99bf490eacc0dd9678aa3f69daaa7ff
fleboulch <florent.le.boulch@gmail.com>
2020-01-15 15:44:20 +0100

    fixup! CSV Reader

--- src/test/java/com/fleboulch/treasuremap/application/exposition/ApplicationFactoryTest.java
+++ src/test/java/com/fleboulch/treasuremap/application/exposition/ApplicationFactoryTest.java
@@ -1,17 +1,16 @@
 package com.fleboulch.treasuremap.application.exposition;
 
 import com.fleboulch.treasuremap.application.domain.TreasureQuest;
-import com.fleboulch.treasuremap.kernel.utils.ConverterUtils;
 import com.fleboulch.treasuremap.map.domain.*;
 import com.fleboulch.treasuremap.map.domain.exceptions.BoxIsOutOfMapException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationFactoryTest {
 
