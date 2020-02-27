13a5c69e3a2141f3915f18f3776b953d49bad352
fleboulch <florent.le.boulch@gmail.com>
2020-02-27 11:38:55 +0100

    Replace if with switch case

--- src/main/java/com/fleboulch/treasuremap/explorer/domain/MovementType.java
+++ src/main/java/com/fleboulch/treasuremap/explorer/domain/MovementType.java
@@ -1,22 +1,20 @@
 package com.fleboulch.treasuremap.explorer.domain;
 
-import java.util.Objects;
-
 public enum MovementType {
     A,
     G,
     D;
 
     public Position executeMovement(Position position) {
-        if (Objects.equals(this, MovementType.D)) {
-            return new Position(position.orientation().rightTurn(), position.coordinates());
-        } else if (Objects.equals(this, MovementType.G)) {
-            return new Position(position.orientation().leftTurn(), position.coordinates());
-        } else if (Objects.equals(this, MovementType.A)) {
-            return position.goForward();
-        } else {
-            throw new IllegalArgumentException("Unknown movement type");
-
+        switch (this) {
+            case A:
+                return position.goForward();
+            case G:
+                return new Position(position.orientation().leftTurn(), position.coordinates());
+            case D:
+                return new Position(position.orientation().rightTurn(), position.coordinates());
+            default:
+                throw new IllegalArgumentException("Unknown movement type");
         }
     }
 
