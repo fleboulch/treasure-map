9684b81ed56baeedcb6935dfa8b23123c480e071
fleboulch <florent.le.boulch@gmail.com>
2020-02-28 12:15:15 +0100

    Re add disabled tests

--- src/test/java/com/fleboulch/treasuremap/explorer/domain/ExplorerTest.java
+++ src/test/java/com/fleboulch/treasuremap/explorer/domain/ExplorerTest.java
@@ -118,25 +118,23 @@ class ExplorerTest {
     }
 
     @Test
-    @Disabled
     void explorer_turn_D_from_north(
             @ExplorerConfiguration(xCoordinates = 0, yCoordinates = 0, orientationType = OrientationType.N, movements = MovementType.D) Explorer beginExplorer,
             @ExplorerConfiguration(xCoordinates = 0, yCoordinates = 0, movements = MovementType.D) Explorer expectedEndExplorer
     ) {
-//        Explorer explorerAfterAction = beginExplorer.turn(MovementType.D);
-//
-//        assertThat(explorerAfterAction).isEqualTo(expectedEndExplorer);
+        Explorer explorerAfterAction = beginExplorer.turn(MovementType.D);
+
+        assertThat(explorerAfterAction).isEqualTo(expectedEndExplorer);
     }
 
     @Test
-    @Disabled
     void explorer_turn_L_from_north(
             @ExplorerConfiguration(xCoordinates = 0, yCoordinates = 0, orientationType = OrientationType.N, movements = MovementType.G) Explorer beginExplorer,
             @ExplorerConfiguration(xCoordinates = 0, yCoordinates = 0, orientationType = OrientationType.W, movements = MovementType.G) Explorer expectedEndExplorer
     ) {
-//        Explorer explorerAfterAction = beginExplorer.turn(MovementType.G);
-//
-//        assertThat(explorerAfterAction).isEqualTo(expectedEndExplorer);
+        Explorer explorerAfterAction = beginExplorer.turn(MovementType.G);
+
+        assertThat(explorerAfterAction).isEqualTo(expectedEndExplorer);
     }
 
     @Test
