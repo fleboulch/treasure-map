d7a0d2c9c1401dc9556edfc68df8790dbc539a1c
fleboulch <florent.le.boulch@gmail.com>
2020-03-21 17:05:41 +0100

    Add one IT test for checking app startup

---
+++ src/test/java/com/fleboulch/treasuremap/TreasureMapApplicationIntTest.java
@@ -0,0 +1,13 @@
+package com.fleboulch.treasuremap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TreasureMapApplicationIntTest {
+
+    @Test
+    void context_should_load_properly() {
+
+    }
+}
